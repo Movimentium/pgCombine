73603463467d3e8dcea6ea919b34b214b23e7c5b
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-03-10 02:19:56 +0100

    C2_02 Publishers Just added

---
+++ pgCombine.playground/Pages/C2_02 Publishers Just.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ # C2_02: Publishers: Just
+ **Publishers** distributes elements to 1 or N Subscribers that it accepts, through the extension of Operators, to create data event chains.
+ */
+import Foundation
+import Combine
+
+// 1. A simple publisher using Just, to produce once for each subscriber
+let _ = Just("Hola mundo")
+    .sink { valor in
+        print("valor: \(valor)")
+    }
+
+
+// 2. Taking advantage of NotificationCenter's publisher
+let notifCenter = NotificationCenter.default
+let notif = Notification(name: .NSSystemClockDidChange)
+let notifClockPublisher = notifCenter.publisher(for: .NSSystemClockDidChange)
+    .sink { valor in
+        print("valor: \(valor)")
+    }
+
+notifCenter.post(notif)
+
+//: [Next](@next)
